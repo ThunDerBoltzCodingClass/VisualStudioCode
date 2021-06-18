407d5cb1ae733f41cdd727c42dac2d84c0edf578
ThunDerBoltzCodingClass <76979208+ThunDerBoltzCodingClass@users.noreply.github.com>
2021-06-18 14:39:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name= "ThunDer";
+var last_name= "Boltz";
+console.log(first_name.concat(last_name));
+var a= 4;
+var b= 2;
+var square= Math.sqrt(a,b);
+var sub= Math.max(a,b);
+var multiply= Math.min(a,b);
+var pow= Math.pow(a,b);
+console.log(square)
+console.log(sub)
+console.log(multiply)
+console.log(pow)
